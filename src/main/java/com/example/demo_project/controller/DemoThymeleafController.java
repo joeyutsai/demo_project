b8b9605f2f97b5d0d0ec9a522a72b52733751350
yu <yu@example>
2022-12-14 12:01:48 +0800

    SpringBoot Thymeleaf and Security

---
+++ src/main/java/com/example/demo_project/controller/DemoThymeleafController.java
@@ -0,0 +1,17 @@
+package com.example.demo_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DemoThymeleafController {
+
+	@RequestMapping(value = "/hello")
+	public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "hello.html";
+	}
+}
